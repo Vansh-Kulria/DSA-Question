ea09c611e2ff6c23c05ffe6789d40a6293701df5
Vansh-Kulria <vanshkulria05@gmail.com>
2026-07-10 02:46:33 +0530

    Time: 0 ms (100%), Space: 10.2 MB (78.69%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -5,7 +5,7 @@ public:
         int prev = nums[0];
         int prev2 = 0;
 
-        for(int i = 0; i < n; i++){
+        for(int i = 1; i < n; i++){
             int take = nums[i];
             if(i > 1){
                 take+=prev2;
