9234da4ececbd3b0858365dd93b989be4739693c
Vansh-Kulria <vanshkulria05@gmail.com>
2026-07-15 05:53:45 +0530

    Time: 179 ms (16.91%), Space: 17.5 MB (59.59%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,22 +1,68 @@
+// class Solution {
+// public:
+//     bool solve(vector<int>& nums, int target, int index,
+//                vector<vector<int>>& dp) {
+//         if (target == 0)
+//             return true;
+
+//         if (index == nums.size())
+//             return false;
+//         if (dp[index][target] != -1)
+//             return dp[index][target];
+//         bool inc = solve(nums, target - nums[index], index + 1, dp);
+//         bool exc = solve(nums, target, index + 1, dp);
+
+//         return dp[index][target] = inc || exc;
+//     }
+//     bool canPartition(vector<int>& nums) {
+//         int sum = accumulate(nums.begin(), nums.end(), 0);
+//         int n = nums.size();
+//         if (sum & 1) {
+//             return false;
+//         }
+//         int target = sum >> 1;
+//         vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+//         return solve(nums, target, 0, dp);
+//     }
+// };
+
+
+
+// tabulation method
 class Solution {
 public:
-bool solve(vector<int>& nums, int target , int index, vector<vector<int>>& dp){
-    if(target < 0 || index == nums.size()) return false;
-    if(target == 0) return true;
-    if(dp[index][target] != -1 ) return dp[index][target];
-    bool inc = solve(nums,target-nums[index], index +1, dp);
-    bool exc = solve(nums , target, index+1,dp);
-
-    return dp[index][target] = inc || exc;
-}
     bool canPartition(vector<int>& nums) {
+
         int sum = accumulate(nums.begin(), nums.end(), 0);
-        int n = nums.size();
-        if(sum & 1){
+
+        if(sum % 2)
             return false;
+
+        int target = sum / 2;
+        int n = nums.size();
+
+        vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
+
+        // Base case: target = 0
+        for(int i = 0; i <= n; i++)
+            dp[i][0] = true;
+
+        // Fill table
+        for(int index = n - 1; index >= 0; index--) {
+
+            for(int t = 1; t <= target; t++) {
+
+                bool notTake = dp[index + 1][t];
+
+                bool take = false;
+
+                if(t >= nums[index])
+                    take = dp[index + 1][t - nums[index]];
+
+                dp[index][t] = take || notTake;
+            }
         }
-        int target = sum >> 1;
-        vector<vector<int>>dp(n, vector<int>(target+1,-1));
-        return solve(nums, target , 0 ,dp);
-     }
+
+        return dp[0][target];
+    }
 };
