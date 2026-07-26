e68566d8e6e96b01c3372d6917c5407927f7e55e
Vansh-Kulria <vanshkulria05@gmail.com>
2026-07-27 04:29:17 +0530

    Time: 458 ms (20.66%), Space: 225.4 MB (13.05%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    int f(vector<int>& prices, int ind, int cup, int buy, int& n,
+          vector<vector<vector<int>>>& dp) {
+        // base case
+        if (ind == n)
+            return 0;
+        if (cup == 0)
+            return 0;
+
+        if (dp[ind][buy][cup] != -1)
+            return dp[ind][buy][cup];
+        if (buy && cup) {
+            return dp[ind][buy][cup] =
+                       max(-prices[ind] + f(prices, ind + 1, cup, 0, n, dp),
+                           0 + f(prices, ind + 1, cup, buy, n, dp));
+        } else {
+            return dp[ind][buy][cup] =
+                       max(prices[ind] + f(prices, ind + 1, cup - 1, 1, n, dp),
+                           0 + f(prices, ind + 1, cup, buy, n, dp));
+        }
+    }
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        vector<vector<vector<int>>> dp(
+            n, vector<vector<int>>(2, vector<int>(3, -1)));
+        return f(prices, 0, 2, 1, n, dp);
+    }
+};
+
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+
+//         int n = prices.size();
+
+//         vector<vector<int>> dp(n + 1, vector<int>(2, 0));
+
+//         for (int ind = n - 1; ind >= 0; ind--) {
+
+//             // buy = 1
+//             dp[ind][1] = max(-prices[ind] + dp[ind + 1][0],
+//                               dp[ind + 1][1]);
+
+//             // buy = 0
+//             dp[ind][0] = max(prices[ind] + dp[ind + 1][1],
+//                               dp[ind + 1][0]);
+//         }
+
+//         return dp[0][1];
+//     }
+// };
+
+// class Solution {
+// public:
+//     int maxProfit(vector<int>& prices) {
+
+//         int n = prices.size();
+
+//         vector<int> ahead(2, 0), curr(2, 0);
+
+//         for (int ind = n - 1; ind >= 0; ind--) {
+
+//             curr[1] = max(-prices[ind] + ahead[0],
+//                           ahead[1]);
+
+//             curr[0] = max(prices[ind] + ahead[1],
+//                           ahead[0]);
+
+//             ahead = curr;
+//         }
+
+//         return ahead[1];
+//     }
+// };
