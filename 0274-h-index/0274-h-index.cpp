b061e84beead3eb75ed9fb4aabadfca50be88d04
Vansh-Kulria <vanshkulria05@gmail.com>
2026-07-28 12:52:49 +0530

    Time: 0 ms (100%), Space: 12.2 MB (63.92%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -1,17 +1,31 @@
+// class Solution {
+// public:
+//     int hIndex(vector<int>& citations) {
+//         int n = citations.size();
+//         for (int h = n; h >= 0; h--) {
+//             int cnt = 0;
+
+//             for (int x : citations) {
+//                 if (x >= h)
+//                     cnt++;
+//             }
+
+//             if (cnt >= h) {
+//                 return h;
+//             }
+//         }
+//         return 0;
+//     }
+// };
+
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
         int n = citations.size();
-        for (int h = n; h >= 0; h--) {
-            int cnt = 0;
-
-            for (int x : citations) {
-                if (x >= h)
-                    cnt++;
-            }
-
-            if (cnt >= h) {
-                return h;
+        sort(citations.begin(), citations.end());
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= n - i) {
+                return n - i;
             }
         }
         return 0;
