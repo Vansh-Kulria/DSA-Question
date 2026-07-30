a44e6bc696c1b72f1d612ae98b55297916f62d30
Vansh-Kulria <vanshkulria05@gmail.com>
2026-07-30 18:16:34 +0530

    Time: 3 ms (79.1%), Space: 14.1 MB (87.5%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -70,21 +70,45 @@
 
 
 
-// differant approach 
+// // differant approach 
 
+// class Solution {
+// public:
+// int lengthOfLIS(vector<int>& nums) {
+//     int n = nums.size();
+//     vector<int>dp(n,1);
+//     int maxi = 1;
+//     for(int ind = 0; ind <= n-1; ind++){
+//         for(int prev = 0; prev <= ind-1; prev++){
+//             if(nums[ind] > nums[prev]){
+//                 dp[ind] = max(1+dp[prev], dp[ind]);
+//             }
+//         }
+//         maxi = max(dp[ind], maxi);
+//     }
+//     return maxi;
+// }};
+
+
+
+
+
+// binary search - only used for len of LIS
 class Solution {
 public:
 int lengthOfLIS(vector<int>& nums) {
     int n = nums.size();
-    vector<int>dp(n,1);
-    int maxi = 1;
-    for(int ind = 0; ind <= n-1; ind++){
-        for(int prev = 0; prev <= ind-1; prev++){
-            if(nums[ind] > nums[prev]){
-                dp[ind] = max(1+dp[prev], dp[ind]);
-            }
+
+    vector<int> temp;
+    temp.push_back(nums[0]);
+    for(int i = 1; i < n; i++){
+        if(nums[i] > temp.back()){
+            temp.push_back(nums[i]);
         }
-        maxi = max(dp[ind], maxi);
+        else{
+            int ind = lower_bound(temp.begin(),temp.end(), nums[i]) - temp.begin();
+            temp[ind] = nums[i];
+        }
     }
-    return maxi;
+    return temp.size();
 }};
